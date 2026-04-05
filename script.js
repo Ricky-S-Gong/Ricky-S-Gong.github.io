a5e9412a166ef40fd10e404290f18200b5988c81
Ricky-S-Gong <shangyu2@illinois.edu>
2026-04-05 14:33:17 -0400

    Escape less-than in autoregressive math

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ const siteData = {
   ],
 };
 
-const assetVersion = "20260405-home-tabs-44";
+const assetVersion = "20260405-home-tabs-45";
 const projectCatalog = window.projectCatalog || { categories: [], projects: [] };
 const realProjectCovers = {
   "minimum-wage-unemployment": {
