58c260e7df9e92c1069d129e146ac2a3fa3beae1
Ricky-S-Gong <shangyu2@illinois.edu>
2026-04-05 14:48:28 -0400

    Center detail page reading column

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ const siteData = {
   ],
 };
 
-const assetVersion = "20260405-home-tabs-46";
+const assetVersion = "20260405-home-tabs-47";
 const projectCatalog = window.projectCatalog || { categories: [], projects: [] };
 const realProjectCovers = {
   "minimum-wage-unemployment": {
