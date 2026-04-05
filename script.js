1984a07fc3b4a74a035f8e0ec9f0498e1d45798a
Ricky-S-Gong <shangyu2@illinois.edu>
2026-04-05 13:52:28 -0400

    Define formula variables and remove F1 metric equation

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ const siteData = {
   ],
 };
 
-const assetVersion = "20260405-home-tabs-40";
+const assetVersion = "20260405-home-tabs-41";
 const projectCatalog = window.projectCatalog || { categories: [], projects: [] };
 const realProjectCovers = {
   "minimum-wage-unemployment": {
