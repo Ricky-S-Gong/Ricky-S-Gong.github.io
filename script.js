f64435e95f7163c035ff2d9e3e90a7a803e6dc35
Ricky-S-Gong <shangyu2@illinois.edu>
2026-04-05 14:43:27 -0400

    Refine detail page reading hierarchy

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ const siteData = {
   ],
 };
 
-const assetVersion = "20260405-home-tabs-45";
+const assetVersion = "20260405-home-tabs-46";
 const projectCatalog = window.projectCatalog || { categories: [], projects: [] };
 const realProjectCovers = {
   "minimum-wage-unemployment": {
