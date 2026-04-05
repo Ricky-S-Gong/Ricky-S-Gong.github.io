1c1fd6d3c7cf1364b5b291616ce0bcff30d57a96
Ricky-S-Gong <shangyu2@illinois.edu>
2026-04-05 13:29:13 -0400

    Adjust projects header spacing

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ const siteData = {
   ],
 };
 
-const assetVersion = "20260405-home-tabs-39";
+const assetVersion = "20260405-home-tabs-40";
 const projectCatalog = window.projectCatalog || { categories: [], projects: [] };
 const realProjectCovers = {
   "minimum-wage-unemployment": {
