6725b119febe67650a9205bb46c8aedecd151099
Ricky-S-Gong <shangyu2@illinois.edu>
2026-04-05 13:24:28 -0400

    Align project category headings

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ const siteData = {
   ],
 };
 
-const assetVersion = "20260405-home-tabs-38";
+const assetVersion = "20260405-home-tabs-39";
 const projectCatalog = window.projectCatalog || { categories: [], projects: [] };
 const realProjectCovers = {
   "minimum-wage-unemployment": {
