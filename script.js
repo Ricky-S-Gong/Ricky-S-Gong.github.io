0e4989c10f84fbbedf94869c6e9f8ca02dd83e4c
Ricky-S-Gong <shangyu2@illinois.edu>
2026-04-05 14:12:48 -0400

    Fix inline LaTeX rendering in detail pages

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ const siteData = {
   ],
 };
 
-const assetVersion = "20260405-home-tabs-41";
+const assetVersion = "20260405-home-tabs-42";
 const projectCatalog = window.projectCatalog || { categories: [], projects: [] };
 const realProjectCovers = {
   "minimum-wage-unemployment": {
