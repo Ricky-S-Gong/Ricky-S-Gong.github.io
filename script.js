751d34549e4e69d72facacdb841c585042e086d7
Ricky-S-Gong <shangyu2@illinois.edu>
2026-04-07 00:28:22 -0400

    Improve project tag contrast

--- script.js
+++ script.js
@@ -321,7 +321,7 @@ const siteChrome = {
   },
 };
 
-const assetVersion = "20260406-home-tabs-117";
+const assetVersion = "20260406-home-tabs-118";
 const projectCatalog = window.projectCatalog || { categories: [], projects: [] };
 const realProjectCovers = {
   "minimum-wage-unemployment": {
