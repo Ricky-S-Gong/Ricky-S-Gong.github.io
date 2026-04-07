3b9ef7d80c4f220839928ade69d3fd9b946fb160
Ricky-S-Gong <shangyu2@illinois.edu>
2026-04-06 21:47:05 -0400

    Widen hero card copy

--- script.js
+++ script.js
@@ -321,7 +321,7 @@ const siteChrome = {
   },
 };
 
-const assetVersion = "20260406-home-tabs-112";
+const assetVersion = "20260406-home-tabs-113";
 const projectCatalog = window.projectCatalog || { categories: [], projects: [] };
 const realProjectCovers = {
   "minimum-wage-unemployment": {
