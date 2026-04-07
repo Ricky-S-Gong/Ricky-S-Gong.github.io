9ad0a109556032951aa473c3deabe57d5e15c0db
Ricky-S-Gong <shangyu2@illinois.edu>
2026-04-07 17:20:18 -0400

    Soften project cover labels

--- script.js
+++ script.js
@@ -321,7 +321,7 @@ const siteChrome = {
   },
 };
 
-const assetVersion = "20260406-home-tabs-133";
+const assetVersion = "20260407-cover-label-1719";
 const projectCatalog = window.projectCatalog || { categories: [], projects: [] };
 const realProjectCovers = {
   "minimum-wage-unemployment": {
