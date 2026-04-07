1e859d35a823e7c7d135799fa10450d6a8a93c10
Ricky-S-Gong <shangyu2@illinois.edu>
2026-04-06 22:14:17 -0400

    Tighten hero card width slightly

--- script.js
+++ script.js
@@ -321,7 +321,7 @@ const siteChrome = {
   },
 };
 
-const assetVersion = "20260406-home-tabs-113";
+const assetVersion = "20260406-home-tabs-114";
 const projectCatalog = window.projectCatalog || { categories: [], projects: [] };
 const realProjectCovers = {
   "minimum-wage-unemployment": {
