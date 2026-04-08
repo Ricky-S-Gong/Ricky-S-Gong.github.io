770ac7e985668b8e74b4acaccb08719dc769e068
Ricky-S-Gong <shangyu2@illinois.edu>
2026-04-08 18:13:15 -0400

    Lift portrait crop higher

--- script.js
+++ script.js
@@ -318,7 +318,7 @@ const siteChrome = {
   },
 };
 
-const assetVersion = "20260408-full-portrait-2-1002";
+const assetVersion = "20260408-full-portrait-2-1014";
 const projectCatalog = window.projectCatalog || { categories: [], projects: [] };
 const realProjectCovers = {
   "minimum-wage-unemployment": {
